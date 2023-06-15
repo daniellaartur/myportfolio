607c9d31687f6bc57c64c2d759c3bdd4379766d8
Daniella <d.artur@student.fontys.nl>
2023-06-15 15:39:12 +0200

    latest changes

--- js/showModal.js
+++
@@ -1,8 +0,0 @@
-var cardDisplay = document.getElementById('showmodel');
-var showBtn = document.getElementById('showBtn');
-
-showBtn.onclick = function(){
-    //when clicking a button, the window should display the model
-
-    cardDisplay.style.display = "block";
-}
